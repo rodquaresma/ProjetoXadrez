dbd8f636a7104ab0f644a9577d1ba18d6fce76e9
Rodrigo Quaresma <rodrigo.quaresma.souza@gmail.com>
2022-06-27 22:00:56 -0300

    Criada a classe Posicao

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -11,6 +7,10 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine(p);
 
         }
     }
